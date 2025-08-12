d293f9f149a220ae90d89d4046983476fde604d7
Kori Russell <korirussell04@gmail.com>
2025-08-12 13:45:22 -0400

    docs: add Swagger bearerAuth security scheme so Authorize button appears

--- src/main/java/com/korirussell/friendly/wager/config/OpenApiConfig.java
+++ src/main/java/com/korirussell/friendly/wager/config/OpenApiConfig.java
@@ -2,9 +2,28 @@ package com.korirussell.friendly.wager.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Friendly Wager API", version = "v1"))
-public class OpenApiConfig {}
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String schemeName = "bearerAuth";
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes(schemeName,
+                        new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName));
+    }
+}
 
